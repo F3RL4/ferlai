e8798bbe6a3dc439b7e5cc4d4f42299f56682740
Edimar Ferla de Almeida <efa.ferla@gmail.com>
2023-12-26 15:06:05 -0300

    Atualização

--- FuncoesLocais/GeraImagem.py
+++ FuncoesLocais/GeraImagem.py
@@ -1,8 +1,7 @@
-import openai
-
 chave = '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'
 ordem = 'Desenhe um foguete viajando para a Lua'
 
+import openai
 openai.api_key = chave
 
 resultado = openai.Image.create(
--- FuncoesLocais/GeraTexto.py
+++ FuncoesLocais/GeraTexto.py
@@ -1,17 +1,5 @@
-import openai
-
 chave = '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'
-ordem = qual a cor do sol?'
+ordem = 'Qual a cor da Lua?'
 
-openai.api_key = chave
+import openai 
 
-resultado = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=ordem,
-  max_tokens=100,
-  temperature=0
-)
-
-resultado_final = resultado['choices'][0]['text']
-
-print(resultado_final)
